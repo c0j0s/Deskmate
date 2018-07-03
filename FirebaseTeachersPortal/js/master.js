1974975b492f71578552603d4e08e5ebfc2fe53e
Jun Sheng@Acer <c.junsheng@hotmail.com>
2018-07-04 01:47:27 +0800

    fix message page issue
    add support for more questions

--- FirebaseTeachersPortal/js/master.js
+++ FirebaseTeachersPortal/js/master.js
@@ -13,4 +13,32 @@ function formatDate(dbDate){
     var r = d.getFullYear() +'/' + d.getMonth() +'/' +d.getDate() + " " + d.getHours() + ":" + d.getSeconds();
     return r;
 }
-		
+        
+function gernerateNewPaper(){
+    let userDB = firebase.database().ref('profile/0001')
+    userDB.once('value', snapshot=>{
+        let userTopicScore = snapshot.val().topicScore
+        let userPastQuestion = [];
+        snapshot.child('homework').forEach(item =>{
+            item.child('questions').forEach(question =>{
+                userPastQuestion.push(question.val())
+            })
+        })
+        let topicScoreArray = Object.values(userTopicScore)
+        let topicScoreArrayKeys = Object.keys(userTopicScore)
+        
+        console.log(topicScoreArrayKeys)
+        let min = Math.min.apply(Math,topicScoreArray);
+        let max = Math.max.apply(Math,topicScoreArray);
+        if(min === 0){
+            min = 1;
+        }
+        console.log(min)
+        for(let i = 0; i<topicScoreArray.length; i++){
+            let score = topicScoreArray[i]/min
+            console.log(score)
+        }
+        console.log(userTopicScore)
+        console.log(userPastQuestion)
+    })
+}
